e704589f14347aa14f5b2d187a835ee1294dcb18
agnes0304 <jiwoochoi0304@gmail.com>
2023-07-12 11:12:01 +0900

    header, footer css position modified

--- coz-shopping/src/components/Header.jsx
+++ coz-shopping/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 
 function Header() {
@@ -16,21 +17,21 @@ function Header() {
         </div>
         <div className="nav-menu" onClick={toggleHandler}>
           <img src="../assets/burger.png" />
+          {isopen && (
+            <nav className="nav-menu-items">
+              <p>jiwoo님, 안녕하세요!</p>
+              <div>
+                <img src="../assets/giftbox.png" />
+                상품리스트 페이지
+              </div>
+              <div>
+                <img src="../assets/star.png" />
+                북마크 페이지
+              </div>
+            </nav>
+          )}
         </div>
       </header>
-      {isopen && (
-        <nav className="nav-menu-items">
-          <p>jiwoo님, 안녕하세요!</p>
-          <div>
-            <img src="../assets/giftbox.png" />
-            상품리스트 페이지
-          </div>
-          <div>
-            <img src="../assets/star.png" />
-            북마크 페이지
-          </div>
-        </nav>
-      )}
     </>
   );
 }
