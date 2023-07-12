32825deab7687d67426bf51d68d2456f576ea21d
agnes0304 <jiwoochoi0304@gmail.com>
2023-07-12 13:48:54 +0900

    header done

--- coz-shopping/src/components/Header.jsx
+++ coz-shopping/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useState } from "react";
-import './Header.css';
-import imgBurger from '../assets/burger.png';
-import imgLogo from '../assets/logo.png';
-import imgGiftbox from '../assets/giftbox.png';
-import imgStar from '../assets/star.png';
+import "./Header.css";
+import imgBurger from "../assets/burger.png";
+import imgLogo from "../assets/logo.png";
+import imgGiftbox from "../assets/giftbox.png";
+import imgStar from "../assets/star.png";
 
 function Header() {
   const [isopen, setIsopen] = useState("false");
@@ -16,25 +16,27 @@ function Header() {
   return (
     <>
       <header>
-        <div className="nav-logo">
-          <img src={imgLogo} />
-          <h1>COZ Shopping</h1>
-        </div>
-        <div className="nav-menu" onClick={toggleHandler}>
-          <img src={imgBurger} />
-          {isopen && (
-            <nav className="nav-menu-items">
-              <p>jiwoo님, 안녕하세요!</p>
-              <div>
-                <img src={imgGiftbox} />
-                상품리스트 페이지
-              </div>
-              <div>
-                <img src={imgStar} />
-                북마크 페이지
-              </div>
-            </nav>
-          )}
+        <div className="main-header-container">
+          <div className="nav-logo">
+            <img src={imgLogo} />
+            <h1>COZ Shopping</h1>
+          </div>
+          <div className="nav-menu" onClick={toggleHandler}>
+            <img className="nav-menu-img_burger" src={imgBurger} />
+            {isopen && (
+              <nav className="nav-menu-items">
+                <p className="nav-menu-item">jiwoo님, 안녕하세요!</p>
+                <div className="nav-menu-item nav-menu-item_border">
+                  <img className="nav-menu-item-icon" src={imgGiftbox} />
+                  <span>상품리스트 페이지</span>
+                </div>
+                <div className="nav-menu-item">
+                  <img className="nav-menu-item-icon" src={imgStar} />
+                  <span>북마크 페이지</span>
+                </div>
+              </nav>
+            )}
+          </div>
         </div>
       </header>
     </>
