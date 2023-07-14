53031608b51df2c34557fd463bffd920343a9658
agnes0304 <jiwoochoi0304@gmail.com>
2023-07-14 14:47:04 +0900

    header hamburger 이외 영역 onClick close event

--- coz-shopping/src/components/Header.jsx
+++ coz-shopping/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import "./Header.css";
 import imgBurger from "../assets/burger.png";
@@ -8,10 +8,23 @@ import imgStar from "../assets/star.png";
 
 function Header() {
   const [isopen, setIsopen] = useState("false");
+  const menuRef = useRef(null);
 
   function toggleHandler() {
     setIsopen(!isopen);
   }
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setIsopen(false);
+      }
+    }
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
@@ -21,7 +34,7 @@ function Header() {
             <img src={imgLogo} />
             <h1>COZ Shopping</h1>
           </div>
-          <div className="nav-menu" onClick={toggleHandler}>
+          <div className="nav-menu" onClick={toggleHandler} ref={menuRef}>
             <img className="nav-menu-img_burger" src={imgBurger} />
             {isopen && (
               <nav className="nav-menu-items">
